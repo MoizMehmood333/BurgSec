23f0cae3ba67c3814bcbc191a8ae9ae2106fe4c1
MoizMehmood333 <muhammadmoizmehmood@gmail.com>
2023-10-30 17:53:40 +0500

    Graphic Design, Application Development, Enterprise Solutions portfolio updated, Mobile Navigation links color issue fixed

--- assets/js/NavbarTrigger.js
+++ assets/js/NavbarTrigger.js
@@ -1,15 +1,21 @@
 window.addEventListener("scroll", function () {
-    var scrollPosition = window.scrollY;
-    var navigationLinks = document.querySelectorAll(".navbar-wrap ul li a");
-    var logo = document.querySelector(".logo img");
+  var scrollPosition = window.scrollY;
+  var navigationLinks = document.querySelectorAll(".navbar-wrap ul li a");
+  var logo = document.querySelector(".logo img");
 
-    for (var i = 0; i < navigationLinks.length; i++) {
-      if (scrollPosition > 100) {
-        navigationLinks[i].classList.add("black-text", "scroll");
-        logo.src = "assets/img/logo/logo.png";
-      } else {
-        navigationLinks[i].classList.remove("black-text", "scroll");
-        logo.src = "assets/img/logo/w_logo.png";
-      }
+  for (var i = 0; i < navigationLinks.length; i++) {
+    if (scrollPosition > 100) {
+      navigationLinks[i].classList.add("black-text", "scroll");
+      logo.src = "assets/img/logo/logo.png";
+    } else {
+      navigationLinks[i].classList.remove("black-text", "scroll");
+      logo.src = "assets/img/logo/w_logo.png";
     }
-  });
+  }
+});
+
+var mobileNavigationLinks = document.querySelectorAll(".menu-outer ul li a");
+
+for (var j = 0; j < mobileNavigationLinks.length; j++) {
+  mobileNavigationLinks[j].classList.add("black-text", "scroll");
+}
